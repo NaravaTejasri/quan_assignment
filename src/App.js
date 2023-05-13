b98c158eeee80a2a844742a2b2b7ef43c96812df
NaravaTejasri <tejasrisony17@gmail.com>
2023-05-13 21:02:38 +0200

    imported HealthTrack file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import HealthTrack from "./pages/healthTrack";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <HealthTrack />
     </div>
   );
 }
